b132b09f0eb1fac314882469d31aaaed86184347
gastonorqueda <gastonorqueda01@hotmail.com>
2021-09-12 19:04:50 -0300

    ej8final

--- .vscode/Modulo3/Ejercicio8.cpp
+++ .vscode/Modulo3/Ejercicio8.cpp
@@ -5,15 +5,20 @@ using namespace std;
 
 main()
 {
+	int busquedaSecuencial(int[8], int);
 	int Vec[] = {0, 2, 1, 3, 5, 6, 4, 8};
-	int i, dato;
+	int dato = 5; // Modifique para realizar la busqueda
+	busquedaSecuencial(Vec, dato);
+}
+
+busquedaSecuencial(int Vec[8], int dato)
+{
+	int i;
 	bool bandera = false;
 
 	//Busqueda
 
 	i = 0;
-
-	dato = 5;   // Modifique para realizar la busqueda
 
 	while ((bandera == false))
 	{
@@ -31,6 +36,6 @@ main()
 	}
 	else
 	{
-		cout << "El numero " << dato << " a sido encontrado en la posicion " << i - 1  << endl;
+		cout << "El numero " << dato << " a sido encontrado en la posicion " << i - 1 << endl;
 	}
 }
